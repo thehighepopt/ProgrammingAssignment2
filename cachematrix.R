cdb25a82b886f2606b6d6940eb00197bbdb60df7
Steve Duffy <thehighepopt@gmail.com>
2014-10-21 21:18:44 -0500

    Rprog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(ginv) m <<- ginv
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Given a matrix 'x', this function checks if there is already an inverse in cache, 
+  ##returns if it so or creates it if not
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      library(MASS)
+      m <- ginv(data)
+      x$setinverse(m)
+      m
 }
